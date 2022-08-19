a0ed55b40f25f3d51e26f3179015659791e3ad14
AlexAegis <alexaegis@gmail.com>
2022-08-19 22:43:59 +0200

    ci: release

--- libs/colony-core/src/plugins/core_client_plugin.rs
+++ libs/colony-core/src/plugins/core_client_plugin.rs
@@ -61,7 +61,7 @@ impl Plugin for CoreClientPlugin {
 // Sets the icon on windows and X11
 fn set_window_icon(windows: NonSend<WinitWindows>) {
 	let primary = windows.get_window(WindowId::primary()).unwrap();
-	let icon_buf = Cursor::new(include_bytes!("../../../../assets/textures/icon.png"));
+	let icon_buf = Cursor::new(include_bytes!("../../../../assets/textures/bevy.png"));
 	if let Ok(image) = image::load(icon_buf, image::ImageFormat::Png) {
 		let image = image.into_rgba8();
 		let (width, height) = image.dimensions();
