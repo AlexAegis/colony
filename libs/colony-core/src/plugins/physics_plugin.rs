1d0f27b668a5899719ddb74519a04c0b8037fc53
AlexAegis <alexaegis@gmail.com>
2022-08-21 23:41:34 +0200

    ci: checkout lfs

--- libs/colony-core/src/plugins/physics_plugin.rs
+++ libs/colony-core/src/plugins/physics_plugin.rs
@@ -5,10 +5,6 @@ pub struct PhysicsPlugin;
 
 impl Plugin for PhysicsPlugin {
 	fn build(&self, app: &mut App) {
-		app.add_plugin(RapierPhysicsPlugin::<NoUserData>::default()) // Add the plugin
-
-
-        
-			; // Optionally define gravity
+		app.add_plugin(RapierPhysicsPlugin::<NoUserData>::default()); // Add the plugin
 	}
 }
